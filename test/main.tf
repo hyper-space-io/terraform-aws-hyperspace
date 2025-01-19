38d942ae830e71de3a31a17bf3e258a2da187cd1
Idan Brody <idan@projectcircle.co.il>
2025-01-19 09:42:12 +0200

    Creating hyperspace terraform module

---
+++ test/main.tf
@@ -0,0 +1,20 @@
+module "infra" {
+  source = "github.com/hyper-space-io/Hyperspace-terraform-module/tree/setup-cluster-tools"
+  version = "setup-cluster-tools"
+  dex_connectors = jsonencode([
+  {
+    type = "github"
+    id   = "github"
+    name = "GitHub"
+    config = {
+      clientID     = "9572f7c8b59a07404dc9"
+      clientSecret = "8074b2a44adb588cf1202e27afe756ccc5cb029d"
+      orgs         = "hyper-space-io"
+    }
+  }
+  ])
+  domain_name = "hyper-space.xyz"
+  environment = "dev-idan"
+  flow_log_group_class = "INFREQUENT_ACCESS"
+  worker_instance_type = ["m5n.large"]
+}

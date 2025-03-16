fa5dbddcd3b96aaf2ebb95d250618552899b7dae
Idan Brody <idan@projectcircle.co.il>
2025-03-16 15:20:00 +0200

    Fixed issues

--- hyperspace/eks.tf
+++ hyperspace/eks.tf
@@ -227,20 +227,20 @@ resource "kubernetes_storage_class" "ebs_sc_gp3" {
 module "iam_iam-assumable-role-with-oidc" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version                       = "~> 5.48.0"
-  for_each                      = { for k, v in local.iam_policies : k => v if lookup(v, "create_assumable_role", false) == true }
+  for_each                      = { for k, v in aws_iam_policy.policies : k => v if lookup(v, "create_assumable_role", false) == true }
   create_role                   = true
   role_name                     = each.value.name
   provider_url                  = module.eks.cluster_oidc_issuer_url
-  role_policy_arns              = [local.iam_policies["${each.key}"].arn]
+  role_policy_arns              = [aws_iam_policy.policies["${each.key}"].arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:${each.value.sa_namespace}:${each.key}"]
 }
 
 module "boto3_irsa" {
   source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  for_each  = { for k, v in local.iam_policies : k => v if lookup(v, "create_cluster_wide_role", false) == true }
+  for_each  = { for k, v in aws_iam_policy.policies : k => v if lookup(v, "create_cluster_wide_role", false) == true }
   role_name = each.value.name
   role_policy_arns = {
-    policy = local.iam_policies["${each.key}"].arn
+    policy = aws_iam_policy.policies["${each.key}"].arn
   }
   assume_role_condition_test = "StringLike"
   oidc_providers = {

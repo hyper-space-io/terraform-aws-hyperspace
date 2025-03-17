dbb5998676e05feee3c01986cc9c39948a117229
Idan Brody <idan@projectcircle.co.il>
2025-03-17 11:49:17 +0200

    wip

--- hyperspace/eks.tf
+++ hyperspace/eks.tf
@@ -165,6 +165,7 @@ module "eks" {
 
 # EBS CSI Driver IRSA 
 module "irsa-ebs-csi" {
+  count = var.create_eks ? 1 : 0
   source                = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version               = "~>5.48.0"
   role_name             = "${local.cluster_name}-ebs-csi"

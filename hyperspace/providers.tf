e44841996ebe649647344b59351588990a911d84
Idan Brody <idan@projectcircle.co.il>
2025-03-20 15:18:37 +0200

    wip

--- hyperspace/providers.tf
+++ hyperspace/providers.tf
@@ -12,22 +12,24 @@ terraform {
 provider "aws" { region = var.aws_region }
 
 provider "kubernetes" {
-  host                   = module.eks[0].cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks[0].cluster_certificate_authority_data)
+  host                   = local.create_eks ? module.eks[0].cluster_endpoint : ""
+  cluster_ca_certificate = local.create_eks ? base64decode(module.eks[0].cluster_certificate_authority_data) : ""
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", module.eks[0].cluster_name]
+    args        = local.create_eks ? ["eks", "get-token", "--cluster-name", module.eks[0].cluster_name] : []
     command     = "aws"
   }
+  ignore_annotations = local.create_eks ? null : ["all"]
+  ignore_labels      = local.create_eks ? null : ["all"]
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks[0].cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks[0].cluster_certificate_authority_data)
+    host                   = local.create_eks ? module.eks[0].cluster_endpoint : ""
+    cluster_ca_certificate = local.create_eks ? base64decode(module.eks[0].cluster_certificate_authority_data) : ""
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks[0].cluster_name]
+      args        = local.create_eks ? ["eks", "get-token", "--cluster-name", module.eks[0].cluster_name] : []
       command     = "aws"
     }
   }

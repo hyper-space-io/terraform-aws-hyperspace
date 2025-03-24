e19b9cd766a7690a3ffb8487f48dde840b35425b
Idan Brody <idan@projectcircle.co.il>
2025-03-24 15:26:16 +0200

    added core-dump velero and exteranl-secrets

---
+++ hyperspace/core-dump.tf
@@ -0,0 +1,31 @@
+locals {
+  dump_release_name = "core-dump"
+}
+resource "helm_release" "core_dump" {
+  count            = local.create_eks ? 1 : 0
+  name             = local.dump_release_name
+  chart            = "${local.dump_release_name}-handler"
+  version          = "~> 9.0.0"
+  repository       = "https://ibm.github.io/core-dump-handler"
+  namespace        = local.dump_release_name
+  create_namespace = true
+  cleanup_on_fail  = true
+  values = [<<EOT
+daemonset:
+  includeCrioExe: true
+  vendor: rhel7
+  s3BucketName: "${local.s3_bucket_arns["core-dump-logs"]}"
+  s3Region: "${var.aws_region}"
+serviceAccount:
+  name: "${local.dump_release_name}"
+  annotations:
+    eks.amazonaws.com/role-arn: "${module.iam_iam-assumable-role-with-oidc["${local.dump_release_name}"].iam_role_arn}"
+tolerations:
+- key: "fpga"
+  operator: "Equal"
+  value: "true"
+  effect: "NoSchedule"
+EOT
+  ]
+  depends_on = [module.eks]
+}

90e84d97547601ee1f2b3153ffe61877c31ee8ae
Idan Brody <idan@projectcircle.co.il>
2025-03-24 15:51:54 +0200

    added ecr token

---
+++ hyperspace/ecr-token.tf
@@ -0,0 +1,10 @@
+resource "helm_release" "ecr_token" {
+  count           = local.create_eks ? 1 : 0
+  name            = "ecr-credentials-sync"
+  chart           = "${path.module}/ecr-credentials-sync"
+  namespace       = "argocd"
+  wait            = true
+  force_update    = true
+  cleanup_on_fail = true
+  depends_on      = [module.eks, module.vpc, aws_route.peering_routes]
+}

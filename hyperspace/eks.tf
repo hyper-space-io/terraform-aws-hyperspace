d28efe74b5a7687bfffd2e813d741a0ab4698b4f
Idan Brody <idan@projectcircle.co.il>
2025-03-27 16:51:20 +0200

    wip

--- hyperspace/eks.tf
+++ hyperspace/eks.tf
@@ -275,7 +275,7 @@ resource "kubernetes_storage_class" "ebs_sc_gp3" {
 module "iam_iam-assumable-role-with-oidc" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version                       = "~> 5.48.0"
-  for_each                      = { for k, v in local.iam_policies : k => v if lookup(v, "create_assumable_role", false) == true }
+  for_each                      = local.create_eks ?{ for k, v in local.iam_policies : k => v if lookup(v, "create_assumable_role", false) == true } : {}
   create_role                   = true
   role_name                     = each.value.name
   provider_url                  = module.eks[0].cluster_oidc_issuer_url
@@ -286,7 +286,7 @@ module "iam_iam-assumable-role-with-oidc" {
 
 module "boto3_irsa" {
   source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  for_each  = { for k, v in local.iam_policies : k => v if lookup(v, "create_cluster_wide_role", false) == true }
+  for_each  = local.create_eks ? { for k, v in local.iam_policies : k => v if lookup(v, "create_cluster_wide_role", false) == true } : {}
   role_name = each.value.name
   role_policy_arns = {
     policy = aws_iam_policy.policies["${each.key}"].arn

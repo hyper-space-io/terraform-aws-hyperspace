bfa7fe7f60b575853f60b5ec427e8d4213d93b6b
Idan Brody <idan@projectcircle.co.il>
2025-03-29 22:55:26 +0300

    wip

--- hyperspace/kube-prometheus-stack.tf
+++ hyperspace/kube-prometheus-stack.tf
@@ -8,6 +8,7 @@ resource "helm_release" "kube_prometheus_stack" {
   chart            = local.prometheus_release_name
   create_namespace = true
   cleanup_on_fail  = true
+  timeout          = 1000
   version          = "68.3.0"
   namespace        = "monitoring"
   repository       = "https://prometheus-community.github.io/helm-charts"

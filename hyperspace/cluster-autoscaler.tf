8c36036dab2bd7ba049731b7228f0773b99679f1
Idan Brody <idan@projectcircle.co.il>
2025-04-03 16:29:24 +0300

    wip

---
+++ hyperspace/cluster-autoscaler.tf
@@ -0,0 +1,29 @@
+locals {
+  autoscaler_release_name = "cluster-autoscaler"
+}
+resource "helm_release" "cluster_autoscaler" {
+  count            = local.create_eks ? 1 : 0
+  chart            = local.autoscaler_release_name
+  namespace        = local.autoscaler_release_name
+  name             = local.autoscaler_release_name
+  create_namespace = true
+  version          = "~> 9.43.2"
+  repository       = "https://kubernetes.github.io/autoscaler"
+  values = [<<EOF
+extraArgs:
+  scale-down-delay-after-add: 30s
+  scale-down-unneeded-time: 10m
+awsRegion: "${var.aws_region}"
+autoDiscovery:
+  clusterName: "${local.cluster_name}"
+rbac:
+  create: true
+  serviceAccount:
+    create: true
+    name: cluster-autoscaler
+    annotations:
+      eks.amazonaws.com/role-arn: "${module.iam_iam-assumable-role-with-oidc["${local.autoscaler_release_name}"].iam_role_arn}"
+EOF
+  ]
+  depends_on = [module.eks]
+}
--- hyperspace/kube-prometheus-stack.tf
+++ hyperspace/kube-prometheus-stack.tf
@@ -1,151 +1,151 @@
-locals {
-  prometheus_release_name = "kube-prometheus-stack"
-}
+# locals {
+#   prometheus_release_name = "kube-prometheus-stack"
+# }
 
-resource "helm_release" "kube_prometheus_stack" {
-  count            = local.create_eks ? 1 : 0
-  name             = local.prometheus_release_name
-  chart            = local.prometheus_release_name
-  create_namespace = true
-  cleanup_on_fail  = true
-  version          = "68.3.0"
-  namespace        = "monitoring"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  values = [<<EOF
-global:
-  imagePullSecrets:
-    - name: "regcred-secret"
+# resource "helm_release" "kube_prometheus_stack" {
+#   count            = local.create_eks ? 1 : 0
+#   name             = local.prometheus_release_name
+#   chart            = local.prometheus_release_name
+#   create_namespace = true
+#   cleanup_on_fail  = true
+#   version          = "68.3.0"
+#   namespace        = "monitoring"
+#   repository       = "https://prometheus-community.github.io/helm-charts"
+#   values = [<<EOF
+# global:
+#   imagePullSecrets:
+#     - name: "regcred-secret"
 
-grafana:
-  ingress:
-    enabled: true
-    ingressClassName: "${local.internal_ingress_class_name}"
-    annotations:
-      cert-manager.io/cluster-issuer: "prod-certmanager"
-      acme.cert-manager.io/http01-edit-in-place: "true"
-      nginx.ingress.kubernetes.io/auth-type: basic
-      nginx.ingress.kubernetes.io/auth-secret: none
-    hosts:
-      - "grafana.${local.internal_domain_name}"
-    tls:
-      - secretName: "monitoring-tls"
-        hosts:
-          - "grafana.${local.internal_domain_name}"
-  persistence:
-    enabled: true
-    size: 10Gi
+# grafana:
+#   ingress:
+#     enabled: true
+#     ingressClassName: "${local.internal_ingress_class_name}"
+#     annotations:
+#       cert-manager.io/cluster-issuer: "prod-certmanager"
+#       acme.cert-manager.io/http01-edit-in-place: "true"
+#       nginx.ingress.kubernetes.io/auth-type: basic
+#       nginx.ingress.kubernetes.io/auth-secret: none
+#     hosts:
+#       - "grafana.${local.internal_domain_name}"
+#     tls:
+#       - secretName: "monitoring-tls"
+#         hosts:
+#           - "grafana.${local.internal_domain_name}"
+#   persistence:
+#     enabled: true
+#     size: 10Gi
 
-additionalDataSources:
-  - name: "loki"
-    type: "loki"
-    access: "proxy"
-    url: "http://loki.monitoring.svc.cluster.local:3100"
-    version: 1
-    isDefault: false
+# additionalDataSources:
+#   - name: "loki"
+#     type: "loki"
+#     access: "proxy"
+#     url: "http://loki.monitoring.svc.cluster.local:3100"
+#     version: 1
+#     isDefault: false
 
-prometheus:
-  prometheusSpec:
-    externalLabels:
-      environment: "${var.environment}-tfc"
-      cluster: "${var.environment}-tfc"
-    additionalScrapeConfigs:
-      - job_name: "otel_collector"
-        scrape_interval: "10s"
-        static_configs:
-          - targets:
-            - "opentelemetry-collector.opentelemetry:9100"
-            - "opentelemetry-collector.opentelemetry:8888"
-    # remoteWrite:
-    #   - url: "https://prometheus.internal.devops-dev.hyper-space.xyz/api/v1/write"
-    #     writeRelabelConfigs:
-    #       - action: "labeldrop"
-    #         regex: "(endpoint|service|prometheus|prometheus_replica)"
-    storageSpec:
-      volumeClaimTemplate:
-        spec:
-          resources:
-            requests:
-              storage: 100Gi
-    retention: 365d
+# prometheus:
+#   prometheusSpec:
+#     externalLabels:
+#       environment: "${var.environment}-tfc"
+#       cluster: "${var.environment}-tfc"
+#     additionalScrapeConfigs:
+#       - job_name: "otel_collector"
+#         scrape_interval: "10s"
+#         static_configs:
+#           - targets:
+#             - "opentelemetry-collector.opentelemetry:9100"
+#             - "opentelemetry-collector.opentelemetry:8888"
+#     # remoteWrite:
+#     #   - url: "https://prometheus.internal.devops-dev.hyper-space.xyz/api/v1/write"
+#     #     writeRelabelConfigs:
+#     #       - action: "labeldrop"
+#     #         regex: "(endpoint|service|prometheus|prometheus_replica)"
+#     storageSpec:
+#       volumeClaimTemplate:
+#         spec:
+#           resources:
+#             requests:
+#               storage: 100Gi
+#     retention: 365d
 
-alertmanager:
-  enabled: true
+# alertmanager:
+#   enabled: true
 
-kubeEtcd:
-  enabled: false
+# kubeEtcd:
+#   enabled: false
 
-kubeControllerManager:
-  enabled: false
+# kubeControllerManager:
+#   enabled: false
 
-kubeScheduler:
-  enabled: false
-EOF
-  ]
+# kubeScheduler:
+#   enabled: false
+# EOF
+#   ]
 
-  set_sensitive {
-    name  = "grafana.adminPassword"
-    value = random_password.grafana_admin_password.result
-  }
-  depends_on = [module.eks, time_sleep.wait_for_internal_ingress]
-}
+#   set_sensitive {
+#     name  = "grafana.adminPassword"
+#     value = random_password.grafana_admin_password.result
+#   }
+#   depends_on = [module.eks, time_sleep.wait_for_internal_ingress]
+# }
 
-resource "helm_release" "prometheus_adapter" {
-  name       = "prometheus-adapter"
-  version    = "~> 4.11.0"
-  namespace  = "monitoring"
-  chart      = "prometheus-adapter"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  values = [<<EOF
-resources:
-  requests:
-    cpu: "10m"
-    memory: "32Mi"
-prometheus:
-  url: http://"kube-prometheus-stack-prometheus.monitoring.svc"
-EOF
-  ]
-  depends_on = [helm_release.kube_prometheus_stack, module.eks]
-}
+# resource "helm_release" "prometheus_adapter" {
+#   name       = "prometheus-adapter"
+#   version    = "~> 4.11.0"
+#   namespace  = "monitoring"
+#   chart      = "prometheus-adapter"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   values = [<<EOF
+# resources:
+#   requests:
+#     cpu: "10m"
+#     memory: "32Mi"
+# prometheus:
+#   url: http://"kube-prometheus-stack-prometheus.monitoring.svc"
+# EOF
+#   ]
+#   depends_on = [helm_release.kube_prometheus_stack, module.eks]
+# }
 
-resource "random_password" "grafana_admin_password" {
-  length           = 30
-  special          = true
-  override_special = "_%@"
-}
+# resource "random_password" "grafana_admin_password" {
+#   length           = 30
+#   special          = true
+#   override_special = "_%@"
+# }
 
-resource "aws_vpc_endpoint" "prometheus" {
-  count             = var.create_prometheus_private_link && local.create_eks ? 1 : 0
-  vpc_id            = module.vpc.vpc_id
-  service_name      = var.prometheus_endpoint_service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = module.vpc.private_subnets
-  security_group_ids = [aws_security_group.prometheus_endpoint_service[0].id]
-  private_dns_enabled = true
-  ip_address_type     = "ipv4"
-  service_region      = var.prometheus_endpoint_service_region
+# resource "aws_vpc_endpoint" "prometheus" {
+#   count             = var.create_prometheus_private_link && local.create_eks ? 1 : 0
+#   vpc_id            = module.vpc.vpc_id
+#   service_name      = var.prometheus_endpoint_service_name
+#   vpc_endpoint_type = "Interface"
+#   subnet_ids        = module.vpc.private_subnets
+#   security_group_ids = [aws_security_group.prometheus_endpoint_service[0].id]
+#   private_dns_enabled = true
+#   ip_address_type     = "ipv4"
+#   service_region      = var.prometheus_endpoint_service_region
 
-  tags = merge(local.tags, {
-    Name = "Prometheus Endpoint - ${var.project}-${var.environment}"
-  })
-}
+#   tags = merge(local.tags, {
+#     Name = "Prometheus Endpoint - ${var.project}-${var.environment}"
+#   })
+# }
 
-resource "aws_security_group" "prometheus_endpoint_service" {
-  count       = var.create_prometheus_private_link && local.create_eks ? 1 : 0
-  name        = "prometheus-endpoint-service"
-  description = "Security group for prometheus endpoint service"
-  vpc_id      = module.vpc.vpc_id
+# resource "aws_security_group" "prometheus_endpoint_service" {
+#   count       = var.create_prometheus_private_link && local.create_eks ? 1 : 0
+#   name        = "prometheus-endpoint-service"
+#   description = "Security group for prometheus endpoint service"
+#   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = distinct(concat([module.vpc.vpc_cidr_block], jsondecode(var.prometheus_endpoint_additional_cidr_blocks)))
-  }
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = distinct(concat([module.vpc.vpc_cidr_block], jsondecode(var.prometheus_endpoint_additional_cidr_blocks)))
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }

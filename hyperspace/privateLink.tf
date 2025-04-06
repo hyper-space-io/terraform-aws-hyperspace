c3afeee62c153ececcec99719b8fcad02b4d751e
Idan Brody <idan@projectcircle.co.il>
2025-04-06 13:00:12 +0300

    wip

--- hyperspace/privateLink.tf
+++ hyperspace/privateLink.tf
@@ -2,7 +2,7 @@ resource "null_resource" "wait_for_nlb" {
   count = var.enable_argocd ? 1 : 0
   provisioner "local-exec" {
     command = <<EOF
-      until STATE=$(aws elbv2 describe-load-balancers --load-balancer-arns ${data.argocd_lb.nlb.arn} --query 'LoadBalancers[0].State.Code' --output text) && [ "$STATE" = "active" ]; do
+      until STATE=$(aws elbv2 describe-load-balancers --load-balancer-arns ${data.aws_lb.argocd_lb.arn} --query 'LoadBalancers[0].State.Code' --output text) && [ "$STATE" = "active" ]; do
         echo "Waiting for NLB to become active... Current state: $STATE"
         sleep 10
       done
@@ -11,14 +11,14 @@ resource "null_resource" "wait_for_nlb" {
   }
 
   triggers = {
-    nlb_arn = data.aws_lb.nlb.arn
+    nlb_arn = data.aws_lb.argocd_lb.arn
   }
 }
 
 resource "aws_vpc_endpoint_service" "argocd_server" {
   count                      = var.enable_argocd && var.enable_argocd_private_link ? 1 : 0
   acceptance_required        = false
-  network_load_balancer_arns = [data.argocd_lb.nlb.arn]
+  network_load_balancer_arns = [data.aws_lb.argocd_lb.arn]
   allowed_principals         = ["arn:aws:iam::418316469434:root"]
   supported_regions          = [var.aws_region, "eu-central-1"]
   private_dns_name           = "argocd.${var.project}.${local.internal_domain_name}"

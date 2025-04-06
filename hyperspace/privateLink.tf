6bc096c32f408f3884c6837be4082490c33908b2
Idan Brody <idan@projectcircle.co.il>
2025-04-06 16:51:17 +0300

    wip

--- hyperspace/privateLink.tf
+++ hyperspace/privateLink.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "wait_for_nlb" {
-  count = var.enable_argocd ? 1 : 0
+  count = var.enable_argocd && local.create_eks && var.enable_argocd_private_link ? 1 : 0
   provisioner "local-exec" {
     command = <<EOF
       until STATE=$(aws elbv2 describe-load-balancers --load-balancer-arns ${data.aws_lb.argocd_lb[0].arn} --query 'LoadBalancers[0].State.Code' --output text) && [ "$STATE" = "active" ]; do
@@ -16,7 +16,7 @@ resource "null_resource" "wait_for_nlb" {
 }
 
 resource "aws_vpc_endpoint_service" "argocd_server" {
-  count                      = var.enable_argocd && var.enable_argocd_private_link ? 1 : 0
+  count                      = var.enable_argocd && local.create_eks && var.enable_argocd_private_link ? 1 : 0
   acceptance_required        = false
   network_load_balancer_arns = [data.aws_lb.argocd_lb[0].arn]
   allowed_principals         = ["arn:aws:iam::418316469434:root"]

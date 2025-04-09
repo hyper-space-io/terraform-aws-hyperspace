36c0c71876958b1179dedae68713a92f93772808
arielrahamim <ariel@projectcircle.co.il>
2025-04-09 16:17:54 +0300

    WIP

--- Infra-module/app_module.tf
+++ Infra-module/app_module.tf
@@ -36,7 +36,7 @@ resource "tfe_workspace" "app" {
   vcs_repo {
     identifier     = "hyper-space-io/Hyperspace-terraform-module"
     branch         = "simulation"
-    oauth_token_id = data.tfe_workspace.current.vcs_repo[0].oauth_token_id
+    # oauth_token_id = data.tfe_workspace.current.vcs_repo[0].oauth_token_id
   }
   working_directory = "app-module"
 }

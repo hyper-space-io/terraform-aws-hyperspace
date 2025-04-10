0688edf30d32e44c6e1a70754c4126ac8c9ccd7f
arielrahamim <ariel@projectcircle.co.il>
2025-04-10 13:24:18 +0300

    support for both vcs_auth options

--- Infra-module/app_module.tf
+++ Infra-module/app_module.tf
@@ -26,19 +26,20 @@ locals {
     prometheus_endpoint_service_name           = var.prometheus_endpoint_service_name
     prometheus_endpoint_service_region         = var.prometheus_endpoint_service_region
     prometheus_endpoint_additional_cidr_blocks = jsonencode(var.prometheus_endpoint_additional_cidr_blocks)
-
+    }
     # Determine which VCS authentication method to use
     vcs_auth = {
       oauth_token_id             = try(data.tfe_workspace.current.vcs_repo[0].oauth_token_id, "") != "" ? data.tfe_workspace.current.vcs_repo[0].oauth_token_id : null
       github_app_installation_id = try(data.tfe_workspace.current.vcs_repo[0].github_app_installation_id, "") != "" ? data.tfe_workspace.current.vcs_repo[0].github_app_installation_id : null
     }
   }
-}
 
 resource "tfe_workspace" "app" {
   name         = "hyperspace-app-module"
   organization = data.tfe_organizations.all.names[0]
   project_id   = data.tfe_workspace.current.project_id
+  auto_apply   = true
+
   vcs_repo {
     identifier                 = "hyper-space-io/Hyperspace-terraform-module"
     branch                     = "simulation"

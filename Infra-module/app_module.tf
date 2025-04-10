db65e08a67506b94b478d3bc3c0ae58ff92d8b0e
arielrahamim <ariel@projectcircle.co.il>
2025-04-10 13:40:06 +0300

    support for both vcs_auth options

--- Infra-module/app_module.tf
+++ Infra-module/app_module.tf
@@ -38,8 +38,6 @@ resource "tfe_workspace" "app" {
   name         = "hyperspace-app-module"
   organization = data.tfe_organizations.all.names[0]
   project_id   = data.tfe_workspace.current.project_id
-  auto_apply   = true
-
   vcs_repo {
     identifier                 = "hyper-space-io/Hyperspace-terraform-module"
     branch                     = "simulation"

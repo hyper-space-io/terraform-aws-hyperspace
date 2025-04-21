2a0bc5ce0ebad6fb8a41bf4c7af38b6f520451ad
arielrahamim <ariel@projectcircle.co.il>
2025-04-21 09:09:16 +0300

    WIP

--- Infra-module/outputs.tf
+++ Infra-module/outputs.tf
@@ -1,6 +1,7 @@
 #######################
 ###### GENERAL ########
 #######################
+
 output "tags" {
   value       = local.tags
   description = "A map of tags that is applied to all resources created by this Terraform configuration. These tags are used consistently across all modules for resource identification, cost allocation, access control, and operational purposes. They typically include information such as environment, project, and other relevant metadata."
@@ -14,6 +15,7 @@ output "environment" {
 #######################
 ####### AWS ###########
 #######################
+
 output "aws_region" {
   value       = var.aws_region
   description = "The AWS region where the VPC and all associated resources are deployed."
@@ -22,15 +24,8 @@ output "aws_region" {
 #######################
 ######### TFE #########
 #######################
+
 output "tfe_workspace" {
   value       = data.tfe_workspace.current
   description = "The complete object representing the TFE workspace for the app module, including workspace configurations, policies, and associated resources."
-}
-
-#######################
-######### ACM #########
-#######################
-output "acm_certificate_domain_validation_options" {
-  value       = module.acm.acm_certificate_domain_validation_options
-  description = "A list of attributes to feed into other resources to complete certificate validation. Can have more than one element, e.g. if SANs are defined. Only set if DNS-validation was used."
 }
--- app-module/outputs.tf
+++ app-module/outputs.tf
@@ -1,4 +1,3 @@
-
 #######################
 ##### PRIVATELINK #####
 #######################
@@ -9,4 +8,13 @@ output "aws_vpc_endpoint_service_domain_verification_name" {
 
 output "aws_vpc_endpoint_service_domain_verification_value" {
   value = aws_vpc_endpoint_service.argocd_server[0].private_dns_name_configuration[0].value
+}
+
+#######################
+######## ACM ##########
+#######################
+
+output "acm_certificate_domain_validation_options" {
+  value       = module.acm.acm_certificate_domain_validation_options
+  description = "A list of attributes to feed into other resources to complete certificate validation. Can have more than one element, e.g. if SANs are defined. Only set if DNS-validation was used."
 }
